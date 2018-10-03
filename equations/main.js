78e71730b32c1745f15df28acfb9ee72a9c6c720
jdapayne <jdapayne@gmail.com>
2018-10-03 14:17:14 +0100

    Fractions options

--- equations/main.js
+++ equations/main.js
@@ -14,7 +14,10 @@ var answered = false;
 function generateAll() {
   document.getElementById("display-box").innerHTML="";
   questions = [];
-  let n = document.getElementById("n_questions").value;
+  var n = parseInt(document.getElementById("n_questions").value);
+  var mindiff = parseInt(document.getElementById("min_diff").value);
+  var maxdiff = parseInt(document.getElementById("max_diff").value);
+
   for (let i=0; i<n; i++) {
     // Make DOM elements
     let container = document.createElement("div");
@@ -27,14 +30,15 @@ function generateAll() {
       container: container
     });
 
-    generate(i);
+    var difficulty = mindiff + Math.floor(i*(maxdiff-mindiff+1)/n);
+    generate(i,difficulty);
   }
 
   document.getElementById("show-answers").disabled = false;
 }
 
-function generate(i) {
-  var question = makeQuestion(1);
+function generate(i,difficulty) {
+  var question = makeQuestion(difficulty);
   questions[i].question = question;
 
   var questionp = document.createElement("p");
@@ -55,28 +59,55 @@ function generate(i) {
   container.appendChild(answerp);
 }
 
-function makeQuestion(diff) {
+function makeQuestion(difficulty) {
   var m, c, x1, y1, x2, y2;
-  var minm = 1, maxm = 5;
-  var minc = 0, maxc = 10;
-  m = randBetween(minm,maxm);
-  c = randBetween(minc,maxc);
+  var minm, maxm, minc, maxc;
 
-  x1 = randBetween(0,10);
-  y1 = m*x1 + c;
+  switch (difficulty) {
+    case 1: // m>0, c>=0
+    case 2:
+    case 3:
+      minm = difficulty<3? 1 : -5;
+      maxm = 5;
+      minc = difficulty<2? 0 : -10;
+      maxc = 10;
+      m = randBetween(minm,maxm);
+      c = randBetween(minc,maxc);
+      x1 = difficulty<3? randBetween(0,10) : randBetween(-15,15);
+      y1 = m*x1 + c;
 
-  x2 = randBetween(x1+1,15);
-  y2 = m*x2 + c;
+      if (difficulty < 3) {
+        x2 = randBetween(x1+1,15);
+      } else {
+        x2 = x1;
+        while (x2===x1) {x2 = randBetween(-15,15)};
+      }
+      y2 = m*x2 + c;
+      break;
+    case 4: // m fraction, points are integers
+    default:
+      var md = randBetween(1,5);
+      var mn = randBetween(-5,5);
+      m = new Fraction(mn,md);
+      x1 = new Fraction(randBetween(-10,10));
+      y1 = new Fraction(randBetween(-10,10));
+      c = new Fraction(y1).sub(m.mul(x1));
+      x2 = x1.add(randBetween(1,5)*m.d);
+      y2 = m.mul(x2).add(c);
+      break;
+  }
 
   var xstr =
-    m===0  ? "" :
-    m===1  ? "x" :
-    m===-1 ? "-x" :
+    (m===0 || (m.equals && m.equals(0))) ? "" :
+    (m===1 || (m.equals && m.equals(1))) ? "x" :
+    (m===-1 || (m.equals && m.equals(-1))) ? "-x" :
+    (m.toLatex) ? m.toLatex() + "x" :
     (m+"x");
   
   var conststr = // TODO: When m=c=0 
-    c===0 ? "" :
-    c < 0 ? (" - " + -c) :
+    (c===0 || c.equals && c.equals(0)) ? "" :
+    (c < 0) ? (" - " + (c.neg?c.neg().toLatex():-c)) :
+    (c.toLatex) ? (" + " + c.toLatex()) :
     (" + " + c);
 
   var question = "("+x1+", "+y1+")\\text{ and }("+x2+", "+y2+")";
@@ -93,13 +124,11 @@ function randBetween(n,m,dist) {
 function toggleAnswers(e) {
   var answers = document.getElementsByClassName("answer");
   var n = answers.length;
-  debugger;
   if (!answered) {
     for (var i = 0; i<n; i++) {
       answers[i].classList.remove("hidden");
     }
     answered = true;
-    e.target
   } else {
     for (var i = 0; i<n; i++) {
       answers[i].classList.add("hidden");
