5e72ab981bd90c1572f6897f2479bf16a6f68ed1
Jonathan Payne <jdapayne@gmail.com>
2020-12-17 09:10:01 +0000

    update histogram

--- histogram/Histogram.js
+++ histogram/Histogram.js
@@ -1,3 +1,12 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const PADDING_START = 250;
 const PADDING_END = 25;
 const PADDING_TOP = 20;
@@ -181,5 +190,9 @@ export default class Histogram {
     }
     scaledCoords([x, y]) {
         return [this.scaleX(x), this.scaleY(y)];
+    }
+    copyToClipBoard() {
+        return __awaiter(this, void 0, void 0, function* () {
+        });
     }
 }
--- histogram/main.js
+++ histogram/main.js
@@ -1,6 +1,7 @@
 import { createElem } from './createElem.js';
 import FrequencyTable from './FrequencyTable.js';
 import Histogram from './Histogram.js';
+/* Make table and buttons*/
 const tableDiv = document.getElementById('table');
 const frequencyTable = new FrequencyTable('time', 't', 'seconds');
 frequencyTable.addRow();
@@ -15,14 +16,7 @@ createElem('br', undefined, tableDiv);
 const tableCopy = createElem('button', undefined, tableDiv);
 tableCopy.innerHTML = 'Copy formatted table';
 tableDiv.append('(e.g. for pasting into Word)');
-const histogramDiv = document.getElementById('histogram');
-const histogram = new Histogram(frequencyTable, 800, 600);
-histogramDiv.append(histogram.canvas);
-histogram.render();
-createElem('br', undefined, histogramDiv);
-const histogramCopy = createElem('button', undefined, histogramDiv);
-histogramCopy.innerHTML = 'Copy histogram';
-frequencyTable.htmlElement.addEventListener('change', () => { histogram.render(); });
+/* Event listeners for table buttons*/
 addRow.addEventListener('click', () => {
     frequencyTable.addRow();
     histogram.render();
@@ -40,15 +34,43 @@ tableCopy.addEventListener('click', () => {
         window.alert(`Error: ${reason}`);
     });
 });
+/* Make histogram and buttons*/
+const histogramDiv = document.getElementById('histogram');
+const histogram = new Histogram(frequencyTable, 800, 600);
+histogramDiv.append(histogram.canvas);
+histogram.render();
+createElem('br', undefined, histogramDiv);
+const histogramCopy = createElem('button', undefined, histogramDiv);
+histogramCopy.innerHTML = 'Copy histogram';
+const histogramDownload = createElem('button', undefined, histogramDiv);
+histogramDownload.innerHTML = 'Download image';
+/* Make the histogram respond to changes in the table*/
+frequencyTable.htmlElement.addEventListener('change', () => { histogram.render(); });
+/* Event listeners for histogram buttons */
 histogramCopy.addEventListener('click', () => {
-    const blob = histogram.canvas.toBlob(blob => {
-        if (!blob)
-            throw new Error('Could not get blob');
-        const clipItem = new ClipboardItem({ [blob.type]: blob });
-        navigator.clipboard.write([clipItem]).then(() => {
-            window.alert('Copied histogram to clipboard');
-        }, reason => {
-            window.alert(`Error copying: ${reason}`);
+    const blobPromise = new Promise((resolve, reject) => {
+        histogram.canvas.toBlob(blob => {
+            if (!blob)
+                reject();
+            else
+                resolve(blob);
         });
     });
+    blobPromise.then(blob => {
+        const clipItem = new ClipboardItem({ [blob.type]: blob });
+        return clipItem;
+    }).then(clipItem => {
+        return navigator.clipboard.write([clipItem]);
+    }).then(() => {
+        window.alert('Copied histogram to clipboard');
+    }, reason => {
+        window.alert(`Error copying: ${reason}`);
+    });
+});
+histogramDownload.addEventListener('click', () => {
+    const dataURL = histogram.canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
+    const link = document.createElement('a');
+    link.href = dataURL;
+    link.download = 'histogram.png';
+    link.click();
 });
