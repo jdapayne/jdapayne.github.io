4c22e98a5fee7033fd45cb951acacce07c89b24d
jdapayne <jdapayne@gmail.com>
2024-07-29 09:10:02 +0100

    Add update to modulusring

--- modulusring/ModRing.js
+++ modulusring/ModRing.js
@@ -1,12 +1,10 @@
-var ModRing = /** @class */ (function () {
-    function ModRing(width, height, modulus) {
-        if (width === void 0) { width = 500; }
-        if (height === void 0) { height = 500; }
-        if (modulus === void 0) { modulus = 10; }
+export default class ModRing {
+    constructor(width = 500, height = 500, modulus = 10) {
         this.width = 500;
         this.height = 500;
         this.expression = 'n';
         this.modulus = 10;
+        this.singlePath = false;
         this.width = width;
         this.height = height;
     }
@@ -15,47 +13,42 @@ var ModRing = /** @class */ (function () {
      *
      * Expression is valide if it contains only numbers, n, +, -, *, /, and ^
      */
-    ModRing.prototype.validateExpression = function () {
-        var regex = /^[0-9n+\-*/^()]+$/;
+    validateExpression() {
+        const regex = /^[0-9n+\-*/^()]+$/;
         return regex.test(this.expression);
-    };
+    }
     /**
      * Evalueate the expression at the value n.
      * @param n
      */
-    ModRing.prototype.evaluateExpression = function (n) {
+    evaluateExpression(n) {
         this.validateExpression();
-        var expression = this.expression.replace(/(\d+)n/, "$1*n")
+        const expression = this.expression.replace(/(\d+)n/, "$1*n")
             .replace("^", "**");
-        console.log("Evaluting " + expression + " at n=" + n);
-        var evaluation = eval(expression); //urgh
+        console.log(`Evaluting ${expression} at n=${n}`);
+        const evaluation = eval(expression); //urgh
         if (typeof evaluation === 'number') {
             return evaluation;
         }
         else {
             throw new Error('Invalid expression');
         }
-    };
-    ModRing.prototype.drawIn = function (canvas) {
-        var ctx = canvas.getContext('2d');
-        if (!ctx) {
-            throw new Error('Canvas not supported');
-        }
-        var width = canvas.width = this.width;
-        var height = canvas.height = this.height;
-        var center = [width / 2, height / 2];
-        var radius = width * 0.45;
-        ctx.clearRect(0, 0, width, height);
+    }
+    /**
+     * Draws the circle with labels on the canvas
+     * @param ctx CanvasRenderingContext2D The context to draw the circle in
+     */
+    drawCircle(ctx, center, radius) {
         ctx.beginPath();
         ctx.arc(center[0], center[1], radius, 0, 2 * Math.PI);
         ctx.stroke();
         ctx.closePath();
         // Draw points and labels
-        for (var i = 0; i < this.modulus; i++) {
-            var x = radius * Math.cos(2 * Math.PI * i / this.modulus);
-            var y = -radius * Math.sin(2 * Math.PI * i / this.modulus);
-            var x2 = (radius + 10) * Math.cos(2 * Math.PI * i / this.modulus);
-            var y2 = -(radius + 10) * Math.sin(2 * Math.PI * i / this.modulus);
+        for (let i = 0; i < this.modulus; i++) {
+            const x = radius * Math.cos(2 * Math.PI * i / this.modulus);
+            const y = -radius * Math.sin(2 * Math.PI * i / this.modulus);
+            const x2 = (radius + 10) * Math.cos(2 * Math.PI * i / this.modulus);
+            const y2 = -(radius + 10) * Math.sin(2 * Math.PI * i / this.modulus);
             // Draw a dot at the point
             ctx.beginPath();
             ctx.arc(center[0] + x, center[1] + y, 3, 0, 2 * Math.PI);
@@ -65,12 +58,32 @@ var ModRing = /** @class */ (function () {
             ctx.strokeText(i.toString(), center[0] + x2, center[1] + y2);
             ctx.closePath();
         }
-        for (var i = 0; i < this.modulus; i++) {
-            var x = radius * Math.cos(2 * Math.PI * i / this.modulus);
-            var y = -radius * Math.sin(2 * Math.PI * i / this.modulus);
-            var j = this.evaluateExpression(i);
-            var xj = radius * Math.cos(2 * Math.PI * j / this.modulus);
-            var yj = -radius * Math.sin(2 * Math.PI * j / this.modulus);
+    }
+    drawIn(canvas) {
+        const ctx = canvas.getContext('2d');
+        if (!ctx) {
+            throw new Error('Canvas not supported');
+        }
+        const width = canvas.width = this.width;
+        const height = canvas.height = this.height;
+        const center = [width / 2, height / 2];
+        const radius = width * 0.45;
+        ctx.clearRect(0, 0, width, height);
+        this.drawCircle(ctx, center, radius);
+        if (this.singlePath) {
+            this.drawPathIn(ctx, center, radius);
+        }
+        else {
+            this.drawAllIn(ctx, center, radius);
+        }
+    }
+    drawAllIn(ctx, center, radius) {
+        for (let i = 0; i < this.modulus; i++) {
+            const x = radius * Math.cos(2 * Math.PI * i / this.modulus);
+            const y = -radius * Math.sin(2 * Math.PI * i / this.modulus);
+            const j = this.evaluateExpression(i);
+            const xj = radius * Math.cos(2 * Math.PI * j / this.modulus);
+            const yj = -radius * Math.sin(2 * Math.PI * j / this.modulus);
             ctx.strokeStyle = 'red';
             ctx.beginPath();
             ctx.moveTo(center[0] + x, center[0] + y);
@@ -78,8 +91,32 @@ var ModRing = /** @class */ (function () {
             ctx.stroke();
             ctx.closePath();
         }
-    };
-    return ModRing;
-}());
-export default ModRing;
+    }
+    drawPathIn(ctx, center, radius, start = 1) {
+        let path = [];
+        let current = start;
+        while (true) {
+            path.push(current);
+            const next = this.evaluateExpression(current) % this.modulus;
+            if (path.includes(next)) {
+                break;
+            }
+            current = next;
+        }
+        console.log(path);
+        path.forEach(i => {
+            const x = radius * Math.cos(2 * Math.PI * i / this.modulus);
+            const y = -radius * Math.sin(2 * Math.PI * i / this.modulus);
+            const j = this.evaluateExpression(i);
+            const xj = radius * Math.cos(2 * Math.PI * j / this.modulus);
+            const yj = -radius * Math.sin(2 * Math.PI * j / this.modulus);
+            ctx.strokeStyle = 'red';
+            ctx.beginPath();
+            ctx.moveTo(center[0] + x, center[0] + y);
+            ctx.lineTo(center[0] + xj, center[0] + yj);
+            ctx.stroke();
+            ctx.closePath();
+        });
+    }
+}
 //# sourceMappingURL=ModRing.js.map
--- modulusring/main.js
+++ modulusring/main.js
@@ -1,17 +1,20 @@
 import ModRing from './ModRing.js';
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
     var _a;
     // Get the input and canvas elements by their ids
-    var expressionInput = document.getElementById('expression');
-    var nInput = document.getElementById('n');
-    var canvas = document.getElementById('canvas');
-    var modRing = new ModRing();
+    const expressionInput = document.getElementById('expression');
+    const nInput = document.getElementById('n');
+    const singlePath = document.getElementById('path');
+    const canvas = document.getElementById('canvas');
+    const modRing = new ModRing();
     function getInfoAndDraw() {
-        var _a, _b;
-        var expression = (_a = expressionInput === null || expressionInput === void 0 ? void 0 : expressionInput.value) !== null && _a !== void 0 ? _a : 'n';
-        var n = parseInt((_b = nInput === null || nInput === void 0 ? void 0 : nInput.value) !== null && _b !== void 0 ? _b : '10');
+        var _a, _b, _c;
+        const expression = (_a = expressionInput === null || expressionInput === void 0 ? void 0 : expressionInput.value) !== null && _a !== void 0 ? _a : 'n';
+        const n = parseInt((_b = nInput === null || nInput === void 0 ? void 0 : nInput.value) !== null && _b !== void 0 ? _b : '10');
+        const single = (_c = singlePath === null || singlePath === void 0 ? void 0 : singlePath.checked) !== null && _c !== void 0 ? _c : false;
         modRing.expression = expression;
         modRing.modulus = n;
+        modRing.singlePath = single;
         modRing.drawIn(canvas);
     }
     (_a = document.getElementById('form')) === null || _a === void 0 ? void 0 : _a.addEventListener('change', getInfoAndDraw);
