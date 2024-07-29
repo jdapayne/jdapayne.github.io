8e72770631cecd87949a10f7d24fd704a8f363c5
jdapayne <jdapayne@gmail.com>
2024-07-29 14:28:40 +0100

    Update ModRing.js

--- modulusring/ModRing.js
+++ modulusring/ModRing.js
@@ -43,20 +43,24 @@ export default class ModRing {
         ctx.arc(center[0], center[1], radius, 0, 2 * Math.PI);
         ctx.stroke();
         ctx.closePath();
+        const fontSize = this.modulus > 30 ? 35 : 45;
+        const offset = this.modulus > 30 ? 30 : 35;
         // Draw points and labels
         for (let i = 0; i < this.modulus; i++) {
             const x = radius * Math.cos(2 * Math.PI * i / this.modulus);
             const y = -radius * Math.sin(2 * Math.PI * i / this.modulus);
-            const x2 = (radius + 15) * Math.cos(2 * Math.PI * i / this.modulus);
-            const y2 = -(radius + 15) * Math.sin(2 * Math.PI * i / this.modulus);
+            const x2 = (radius + offset) * Math.cos(2 * Math.PI * i / this.modulus);
+            const y2 = -(radius + offset) * Math.sin(2 * Math.PI * i / this.modulus);
             // Draw a dot at the point
             ctx.beginPath();
-            ctx.arc(center[0] + x, center[1] + y, 3, 0, 2 * Math.PI);
+            ctx.arc(center[0] + x, center[1] + y, 6, 0, 2 * Math.PI);
             ctx.fill();
-            ctx.font = '15px ABeeZee, sans-serif';
+            ctx.font = `${fontSize}px "ABeeZee", sans-serif`;
             ctx.textBaseline = 'middle';
             ctx.textAlign = 'center';
-            ctx.fillText(i.toString(), center[0] + x2, center[1] + y2);
+            if (this.modulus < 70) {
+                ctx.fillText(i.toString(), center[0] + x2, center[1] + y2);
+            }
             ctx.closePath();
         }
     }
@@ -65,14 +69,16 @@ export default class ModRing {
         if (!ctx) {
             throw new Error('Canvas not supported');
         }
-        const width = canvas.width = this.width;
-        const height = canvas.height = this.height;
+        const width = canvas.width = this.width * 2;
+        const height = canvas.height = this.height * 2;
+        canvas.style.width = this.width + 'px';
+        canvas.style.height = this.height + 'px';
         const center = [width / 2, height / 2];
-        const radius = width * 0.45;
+        const radius = width * 0.43;
         ctx.clearRect(0, 0, width, height);
         this.drawCircle(ctx, center, radius);
         ctx.strokeStyle = 'red';
-        ctx.lineWidth = 2;
+        ctx.lineWidth = 4;
         if (this.singlePath) {
             this.drawPathIn(ctx, center, radius);
         }
